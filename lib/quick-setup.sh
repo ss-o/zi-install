25bbc1214248d027532d6a130e1a3a5eb32d4f91
Salvydas Lukosius <sall@w-ss.io>
2021-12-02 21:46:20 +0000

    push

--- lib/quick-setup.sh
+++ lib/quick-setup.sh
@@ -7,39 +7,41 @@ SRC_INSTALL="${WORKDIR}/install.sh"
 SRC_INIT="${WORKDIR}/script-init.sh"
 WORKDIR="(mktemp -d)"
 SOURCE="${BASH_SOURCE[0]}"
-if ! command -v git >/dev/null 2>&1; then
-  echo -e "Git is not installed. Please install to proceed."
-  exit 1
-fi
-if ! command -v curl >/dev/null 2>&1; then
-  echo -e "Curl is not installed. Please install to proceed"
-fi
-while [ -h "$SOURCE" ]; do
+GET_SOURCE_FILE() {
+  if ! command -v git >/dev/null 2>&1; then
+    echo -e "Git is not installed. Please install to proceed."
+    exit 1
+  fi
+  if ! command -v curl >/dev/null 2>&1; then
+    echo -e "Curl is not installed. Please install to proceed"
+  fi
+  while [ -h "$SOURCE" ]; do
+    ABSOLUTE_PATH="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$ABSOLUTE_PATH/$SOURCE"
+  done
   ABSOLUTE_PATH="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$ABSOLUTE_PATH/$SOURCE"
-done
-ABSOLUTE_PATH="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-if [[ -f "${ABSOLUTE_PATH}/script-init.sh" ]]; then
-  # Assume that repository cloned with script-init.sh
-  # shellcheck disable=SC1091
-  source "${ABSOLUTE_PATH}/script-init.sh"
-  SET_COLORS
-  MSG_OK "Source script found, proceeding..."
-else
-  if test -d "$WORKDIR"; then
-    curl -fsSL "$SRC_INIT_URL" -o "${SRC_INIT}/script-init.sh"
-    command chmod g-rwX "${SRC_INIT}/script-init.sh"
-    if test -f "${SRC_INIT}/script-init.sh"; then
-      source "${SRC_INIT}/script-init.sh"
-      SET_COLORS
-      MSG_OK "Successfully initialized"
-    else
-      echo "Failed to initialize"
-      exit 1
+  if [[ -f "${ABSOLUTE_PATH}/script-init.sh" ]]; then
+    # Assume that repository cloned with script-init.sh
+    # shellcheck disable=SC1091
+    source "${ABSOLUTE_PATH}/script-init.sh"
+    SET_COLORS
+    MSG_OK "Source script found, proceeding..."
+  else
+    if test -d "$WORKDIR"; then
+      curl -fsSL "$SRC_INIT_URL" -o "${SRC_INIT}/script-init.sh"
+      command chmod g-rwX "${SRC_INIT}/script-init.sh"
+      if test -f "${SRC_INIT}/script-init.sh"; then
+        source "${SRC_INIT}/script-init.sh"
+        SET_COLORS
+        MSG_OK "Successfully initialized"
+      else
+        echo "Failed to initialize"
+        exit 1
+      fi
     fi
   fi
-fi
+}
 
 SHOW_MENU() {
   while true; do
@@ -93,6 +95,7 @@ $TPDIM# =========================================== # $TPRESET
   done
 }
 MAIN() {
+  GET_SOURCE_FILE || exit 1
   SHOW_MENU "${@}"
   exit 0
 }

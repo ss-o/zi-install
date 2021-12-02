16a01fa59576a1cddb9b1813d159780a887a3ad3
Salvydas Lukosius <sall@w-ss.io>
2021-12-02 21:34:27 +0000

    modified:   lib/quick-setup.sh

--- lib/quick-setup.sh
+++ lib/quick-setup.sh
@@ -7,35 +7,38 @@ SRC_INSTALL="${WORKDIR}/install.sh"
 SRC_INIT="${WORKDIR}/script-init.sh"
 WORKDIR="(mktemp -d)"
 SOURCE="${BASH_SOURCE[0]}"
+if ! command -v git >/dev/null 2>&1; then
+  echo -e "Git is not installed. Please install to proceed."
+  exit 1
+fi
+if ! command -v curl >/dev/null 2>&1; then
+  echo -e "Curl is not installed. Please install to proceed"
+fi
 while [ -h "$SOURCE" ]; do
   ABSOLUTE_PATH="$(cd -P "$(dirname "$SOURCE")" && pwd)"
   SOURCE="$(readlink "$SOURCE")"
   [[ $SOURCE != /* ]] && SOURCE="$ABSOLUTE_PATH/$SOURCE"
 done
 ABSOLUTE_PATH="$(cd -P "$(dirname "$SOURCE")" && pwd)"
-
-if [[ ! -f "${ABSOLUTE_PATH}/script-init.sh" ]]; then
-  if test -d "$WORKDIR"; then
-    if command -v curl >/dev/null 2>&1; then
-      curl -fsSL "$SRC_INIT_URL" -o "$SRC_INIT"
-      command chmod g-rwX "$SRC_INIT"
-      source "${SRC_INIT}/script-init.sh"
-      SET_COLORS
-      MSG_OK "Successfully initialized"
-    else
-      echo "curl is not installed"
-      exit 1
-    fi
-  else
-    echo "mktemp failed"
-    exit 1
-  fi
-else
+if [[ -f "${ABSOLUTE_PATH}/script-init.sh" ]]; then
   # Assume that repository cloned with script-init.sh
   # shellcheck disable=SC1091
   source "${ABSOLUTE_PATH}/script-init.sh"
   SET_COLORS
   MSG_OK "Source script found, proceeding..."
+else
+  if test -d "$WORKDIR"; then
+    curl -fsSL "$SRC_INIT_URL" -o "${SRC_INIT}/script-init.sh"
+    command chmod g-rwX "${SRC_INIT}/script-init.sh"
+    if -f "${SRC_INIT}/script-init.sh"; then
+      source "${SRC_INIT}/script-init.sh"
+      SET_COLORS
+      MSG_OK "Successfully initialized"
+    else
+      echo "Failed to initialize"
+      exit 1
+    fi
+  fi
 fi
 
 SHOW_MENU() {

d2ee522554c299173577c21ab1f6350926f06c90
Salvydas Lukosius <sall@w-ss.io>
2021-12-05 10:09:12 +0000

    modified:   lib/quick-setup.sh

--- lib/quick-setup.sh
+++ lib/quick-setup.sh
@@ -2,7 +2,6 @@
 
 [[ -n "$ENABLE_DEBUG_MODE" ]] && set -x
 
-TERM="xterm-256color"
 NO_TTY="${NO_TTY:-no}"
 PIPED="${PIPED:-no}"
 WORKDIR="$(mktemp -d)"
@@ -121,8 +120,7 @@ DO_SELECTION() {
 }
 DO_OPTIONS() {
   if HAS_TERMINAL; then
-    export TERM
-    echo "$TERM"
+    export TERM="xterm-256color"
   fi
   if ! IS_TTY; then
     NO_TTY=yes
